fce2bd45358e5243383d938d14c8bd5d2c17fd02
Sugarcothe <ifeanyivalentine82@gmail.com>
2022-09-23 10:38:10 +0100

    backend fix

---
+++ src/cryptopoko-backend/schemas/currencies.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'currencies',
+    title: 'Currencies',
+    type: 'document',
+    fields: [
+        {
+            name: 'currency',
+            title: 'Currency',
+            type: 'string'
+        },
+        {
+            name: 'buy',
+            title: 'We buy',
+            type: 'string'
+        },
+        {
+            name2: 'sell',
+            title: 'We sell',
+            type: 'string'
+        },
+    ]
+}
